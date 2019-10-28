896c9cdbc33e467d2fd21916012884650bf3e196
yishengwei <jayyi348694356@qq.com>
2019-10-28 13:37:40 +0800

    thread handler branch create

---
+++ app/src/main/java/com/yshen/study/thread/handler/HandlerActivity.kt
@@ -0,0 +1,16 @@
+package com.yshen.study.thread.handler
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yshen.study.R
+
+/**
+ * Created by Yshen
+ * On 2019-10-28
+ */
+class HandlerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_handler)
+    }
+}
