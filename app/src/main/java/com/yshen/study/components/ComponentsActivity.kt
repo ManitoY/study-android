1b8069709c6172d235c5a5bd6ab6af8e5d3a45f2
yishengwei <jayyi348694356@qq.com>
2019-10-30 17:16:14 +0800

    merge develop_components_activity

--- app/src/main/java/com/yshen/study/components/ComponentsActivity.kt
+++ app/src/main/java/com/yshen/study/components/ComponentsActivity.kt
@@ -1,11 +1,21 @@
 package com.yshen.study.components
 
+import android.content.Intent
 import com.yshen.study.R
 import com.yshen.study.base.BaseActivity
+import com.yshen.study.base.Constants
+import com.yshen.study.components.activity.ActivityActivity
+import kotlinx.android.synthetic.main.activity_components.*
 
 class ComponentsActivity: BaseActivity() {
     override fun getLayoutId(): Int = R.layout.activity_components
 
     override fun afterViews() {
+        activityBtn.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra(Constants.TITLE, activityBtn.text)
+            intent.setClass(this, ActivityActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
